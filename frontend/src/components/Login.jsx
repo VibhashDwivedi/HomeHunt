2dfa01176d73ae65d3b23272f3c9853462763870
VibhashDwivedi <vibhashdwi1005@gmail.com>
2024-08-13 12:27:09 +0530

    checking

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -52,6 +52,7 @@ const Login = () => {
         });
 
         const data = await res.json();
+        console.log(data);
         sessionStorage.setItem("user", JSON.stringify(data));
         setCurrentUser(data);
         setLoggedIn(true);
