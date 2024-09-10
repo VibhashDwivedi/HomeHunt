24a285ce484eae1bbe16c85ba05e5cb7a042c9a3
VibhashDwivedi <vibhashdwi1005@gmail.com>
2024-09-10 11:00:07 +0530

    added reload

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const cors = require("cors");
+const axios = require("axios");
 require('dotenv').config({ path: '.env' });
+
+const url = process.env.BACKEND_URL;
+
 
 const app = express();
 const port = 5000;
@@ -20,9 +24,26 @@ app.use("/user", userRouter);
 app.use('/house',houseRouter);
 app.use('/util',utilRouter);
 
+
+
 app.get("/", (req, res) => {
   res.send("hello from the server!!");
 });
+
+const interval = 30000;
+
+function reloadWebsite() {
+  axios.get(url)
+    .then(response => {
+      console.log(`Reloaded at ${new Date().toISOString()}: Status Code ${response.status}`);
+    })
+    .catch(error => {
+      console.error(`Error reloading at ${new Date().toISOString()}:`, error.message);
+    });
+}
+
+setInterval(reloadWebsite, interval);
+
 
 app.listen(5000, () => {
   console.log("server listening on 5000");
